d2b65390244a2d450d007112c05ab967ef7fb39e
Jodie Putrino <j.putrino@f5.com>
2017-01-27 15:04:30 -0700

    fixes #106

--- scripts/test-docs.sh
+++ scripts/test-docs.sh
@@ -4,12 +4,9 @@ set -x
 
 set -e
 
-echo "Building docs with Sphinx"
+echo "Building docs and checking links with Sphinx"
 rm -rf docs/_build
 make -C docs html
-
-echo "Checking links with Sphinx"
-rm -rf docs/_build
 make -C docs linkcheck
 
 echo "Checking grammar and style"
