148c932886a0f05527e6f86ff5d7250a54e3a036
Michael Day <m.day@f5.com>
2017-06-06 15:13:14 -0600

    WIP: add travis

--- build-tools/docker-docs.sh
+++ build-tools/docker-docs.sh
@@ -2,11 +2,10 @@
 
 set -x
 
-: ${DOC_IMG:=docker-registry.pdbld.f5net.com/tools/containthedocs:master}
+: ${DOC_IMG:=f5devcentral/containthedocs:latest}
 
 exec docker run --rm -it \
   -v $PWD:$PWD --workdir $PWD \
   ${DOCKER_RUN_ARGS} \
   -e "LOCAL_USER_ID=$(id -u)" \
   ${DOC_IMG} "$@"
-
